ad3a1372fe5c4599139c810ca14d5ce3bd4c47ee
shubhamkumar0 <shubham.kumar099@gmail.com>
2018-08-14 15:35:01 +0530

    Update sentiment_tweet.py

--- sentiment_tweet.py
+++ sentiment_tweet.py
@@ -2,11 +2,11 @@ import tweepy
 from textblob import TextBlob
 import pandas as pd
 
-consumer_key='UTx1m8vLBNjGqfygeF5KO3o8V'
-consumer_secret= 'x8b3TCToIP0UUNt2NRcn8FqxsoRH5cn6oHCnLAjsyL6HDQgzzD'
+consumer_key='***********'
+consumer_secret= '$$$$$$$$$$$$$$$$$$$$$$$$$'
 
-access_token= '64656251-bqE7PXIA6TrTLj6LtXdMb3BufFgcW7jhmnrTz0HPB'
-access_token_secret= 'sklcJ0Bhm7qr5pXRTC6ew39sZj1iMdlkJbxNJprIv34u9'
+access_token= '%%%%%%%%%%%%%%%%%%'
+access_token_secret= '^^^^^^^^^^^^^^^^^^^^^^^'
 
 auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
